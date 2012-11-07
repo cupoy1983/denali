4bd75b4181a4ffc082247fe9505d1c4693ae8da7
frankie <cupoy1983@gmail.com>
2012-11-07 09:08:28 +0800

    delete install and update files

--- update/Common/table/admin_log.table.php
+++
@@ -1,9 +0,0 @@
-<?php
-$sql = 'REPLACE INTO '.FDB::table($tableTaget).' SELECT *,\'\' FROM '.$oldDB->tableName($tableTaget);
-if(FDB::query($sql,'SILENT') === false)
-{
-	$sql = 'REPLACE INTO '.FDB::table($tableTaget).' SELECT * FROM '.$oldDB->tableName($tableTaget);
-	FDB::query($sql,'SILENT');
-}
-showjsmessage(U('Index/updatetable',array('table'=>$tableIndex + 1)),5);
-?>
--- update/Common/table/current.table.php
+++
@@ -1,6 +0,0 @@
-<?php
-FDB::query('TRUNCATE TABLE '.FDB::table($tableTaget));
-$sql = 'INSERT INTO '.FDB::table($tableTaget).' SELECT * FROM '.$oldDB->tableName($tableTaget);
-FDB::query($sql,'SILENT');
-showjsmessage(U('Index/updatetable',array('table'=>$tableIndex + 1)),5);
-?>
