f8b0c06521ed873ae5ae3e9e2a5b8c3ef53a3141
frankie <cupoy1983@gmail.com>
2013-11-14 18:01:35 +0800

    暂时关闭返利

--- tpl/concise/js/fanli.js
+++ tpl/concise/js/fanli.js
@@ -30,28 +30,24 @@ function getUrlParam(url, paramName) {
 function ItemsConvert(iid) {
 	
 	TOP.api({
-		method : 'taobao.item.get',
+		method : 'taobao.tbk.items.detail.get ',
 		fields : 'iid,detail_url,num_iid,title,nick,type,cid,pic_url,seller_cids,num,list_time,delist_time,stuff_status,location,price,post_fee,express_fee,ems_fee,has_discount,freight_payer,item_img',
-		num_iid : iid
+		num_iids : iid
 	}, function(resp) {
-		if (resp.item) {
+		if (resp.tbk_items) {
 			$("#resultContainer .result").css("display", "block");
-			var result = resp.item;
+			var result = resp.tbk_items.tbk_item[0];
 			var title = result.title;
 			var price = result.price;
 			var picUrl = result.pic_url;
 			var nick = result.nick;
-			var location = result.location.city + " " + result.location.state;
 			var rate = Math.ceil(Math.random()*20);
-			var num = result.num;
 			var item = $("#J_Search").val();
 			var go = "/tgo.php?title=" + encodeURIComponent(title) + "&item=" + encodeURIComponent(item) + "&from=fanli";
 			
 			$("#J_Commission_Rate").attr('v',rate);
 			$("#J_Rate").text(rate);
 			$("#J_Shopname").text(nick);
-			$("#J_Location").text(location);
-			$("#J_Volume").text(num);
 			$("#J_Price").text(price);
 			$("#J_Commission").text(Math.round(rate * price)/100);
 			
